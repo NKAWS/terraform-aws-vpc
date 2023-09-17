cb622af3af4b959f1fcc072f468b44be5077551e
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-17 22:22:26 +0530

    provider

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+  instance_tenancy = local.instance_tenancy
   enable_dns_support = true
   enable_dns_hostnames = true
+
+  tags = var.tags
 
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 
 variable "vpc_cidr" {
   type = string
-  default = "10.0.0.0/16"
+  #default = "10.0.0.0/16"
 }
-variable "vpc_tags" {
+variable "tags" {
   type = map
   default = {}
 }
